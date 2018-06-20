4f36d250104363fcbc134321085e290f9f1f0adc
wajidch <wajidjaved17@gmail.com>
2018-06-20 21:16:44 +0500

    CRUD for account

---
+++ AccountInv/Account.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Account.aspx.cs" Inherits="AccountInv.Account" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label ID="lblphone" runat="server" Text="Phone"></asp:Label>
+    <asp:TextBox ID="txtphone" runat="server"></asp:TextBox>
+     <asp:Label ID="lblname" runat="server" Text="Name"></asp:Label>
+    <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+     <asp:Label ID="lbltype" runat="server" Text="Account type"></asp:Label>
+    <asp:TextBox ID="txttype" runat="server"></asp:TextBox>
+    <asp:Label ID="lblAddress" runat="server" Text="Address"></asp:Label>
+    <asp:TextBox ID="txtaddress" runat="server"></asp:TextBox>
+    <br />
+    <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" Text="Add" />
+    <asp:Label ID="lblsuccess" ForeColor="Green" runat="server" Text=""></asp:Label>
+    <asp:GridView ID="gridaccount" runat="server" AutoGenerateColumns="false" BackColor="White" BorderColor="#CC9966" BorderStyle="None" BorderWidth="1px" CellPadding="4">
+        <Columns>
+            <asp:TemplateField>
+                <ItemTemplate>
+                    <asp:Label ID="lblphone" runat="server" Text="<%#Eval("Phone") %>"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+                 <asp:TemplateField>
+                <ItemTemplate>
+                       <asp:Label ID="lblname" runat="server" Text="<%#Eval("Name") %>"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:TemplateField>
+                <ItemTemplate>
+                    <asp:Label ID="lblaccountType" runat="server" Text="<%#Eval("Type") %>"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+              <asp:TemplateField>
+                <ItemTemplate>
+                      <asp:Label ID="lbladdress" runat="server" Text="<%#Eval("Address") %>"></asp:Label>
+                </ItemTemplate>
+            </asp:TemplateField>
+             <asp:TemplateField>
+                <ItemTemplate>
+                    <asp:Button ID="btnUpdate" OnCommand="btnUpdate_Command" CommandArgument="<%#Eval("ID") %>" runat="server" Text="Update" />
+                    
+                </ItemTemplate>
+            </asp:TemplateField>
+             <asp:TemplateField>
+                <ItemTemplate>
+                <asp:Button ID="btndelete" OnCommand="btndelete_Command" CommandArgument="<%#Eval("ID") %>" runat="server" Text="Delete" />
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+        <FooterStyle BackColor="#FFFFCC" ForeColor="#330099" />
+        <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="#FFFFCC" />
+        <PagerStyle BackColor="#FFFFCC" ForeColor="#330099" HorizontalAlign="Center" />
+        <RowStyle BackColor="White" ForeColor="#330099" />
+        <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="#663399" />
+        <SortedAscendingCellStyle BackColor="#FEFCEB" />
+        <SortedAscendingHeaderStyle BackColor="#AF0101" />
+        <SortedDescendingCellStyle BackColor="#F6F0C0" />
+        <SortedDescendingHeaderStyle BackColor="#7E0000" />
+    </asp:GridView>
+</asp:Content>
---
+++ AccountInv/Account.aspx.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AccountInv
+{
+    public partial class Account : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Connection"].ToString());
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            AccountInfo();
+        }
+
+        public void AccountInfo()
+        {
+                con.Open();
+                string constring = "select * from Account where Active=true";
+                SqlCommand cmd = new SqlCommand(constring, con);
+                cmd.CommandType = CommandType.Text;
+
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                sda.Fill(ds);
+                gridaccount.Columns[0].HeaderText = "Phone";
+                gridaccount.Columns[1].HeaderText = "Name";
+                gridaccount.Columns[2].HeaderText = "Account Type";
+                gridaccount.Columns[3].HeaderText = "Address";
+                //GridView1.Columns[2].HeaderText = "Date";
+                gridaccount.DataSource = ds.Tables[0].DefaultView;
+                gridaccount.DataBind();
+                con.Close();
+           
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(Session["Update"] !=null){
+             string name=txtname.Text;
+            string phone=txtphone.Text;
+            string address=txtaddress.Text;
+            string accountType=txttype.Text;
+                 con.Open();
+                int id=Convert.ToInt32(Session["id"]);
+                string constring = "update Account set Name="+name+",Phone="+phone+",Type="+accountType+",Address="+address+"  where ID="+id;
+                SqlCommand cmd = new SqlCommand(constring, con);
+                cmd.CommandType = CommandType.Text;
+
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                sda.Fill(ds);
+                Session.Abandon();
+                con.Close();
+            }
+            else
+            {
+            string name=txtname.Text;
+            string phone=txtphone.Text;
+            string address=txtaddress.Text;
+            string accountType=txttype.Text;
+           con.Open();
+            string conString="insert into Account(Name,Type,Address,Phone,Active) values("+name+","+accountType+","+address+","+phone+",true)";
+                SqlCommand cmd = new SqlCommand(conString, con);
+                cmd.CommandType = CommandType.Text;
+              
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    lblsuccess.Text="Data added";
+                   
+                }
+                con.Close();
+        }
+           
+        }
+
+     
+
+        protected void btnUpdate_Command(object sender, CommandEventArgs e)
+        {
+              int id=Convert.ToInt32(e.CommandArgument);
+            Session["id"]=id;
+               con.Open();
+                string constring = "select * from Account where ID="+id;
+                SqlCommand cmd = new SqlCommand(constring, con);
+                cmd.CommandType = CommandType.Text;
+
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+              DataTable dt = new DataTable();
+                sda.Fill(dt);
+             foreach (DataRow dr in dt.Rows)
+                {
+                 //Data poplate in textboxes after that user can edit and update
+                    txtname.Text = dr["Name"].ToString();
+                    txttype.Text = dr["Type"].ToString();
+                    txtaddress.Text = dr["Address"].ToString();
+                    txtphone.Text = dr["Phone"].ToString();
+                 
+
+
+                }
+            Session["Update"] =true;
+            btnAdd.Text="Update";
+            con.Close();
+             
+        }
+
+        protected void btndelete_Command(object sender, CommandEventArgs e)
+        {
+                con.Open();
+       
+                int id=Convert.ToInt32(e.CommandArgument);
+                string constring = "update Account set Active=false where ID="+id;
+                SqlCommand cmd = new SqlCommand(constring, con);
+                cmd.CommandType = CommandType.Text;
+
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                sda.Fill(ds);
+         con.Close();
+             AccountInfo();
+               
+             
+        }
+    }
+}
