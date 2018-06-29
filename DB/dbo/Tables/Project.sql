5afd90a6cacdc23a080a9e2636db5ba71b94de62
thezeeshanasghar <thezeeshanasghar@gmail.com>
2018-06-29 10:20:51 +0500

    add Project table

---
+++ DB/dbo/Tables/Project.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Project]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Code] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(100) NULL, 
+    [Location] NVARCHAR(100) NOT NULL, 
+    [Responsible] NVARCHAR(50) NOT NULL
+)
