9c44ca16b94f0b4bcf36a15ae6fc66c8fe21dd8c
Muneeb Ur Rehman <munni7474@gmail.com>
2018-06-30 17:41:41 +0500

    Fix db project primary key auto increment issue

--- DB/dbo/Tables/Project.sql
+++ DB/dbo/Tables/Project.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[Project]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[ID] INT IDENTITY (1, 1) NOT NULL, 
     [Code] NVARCHAR(50) NOT NULL, 
     [Description] NVARCHAR(100) NULL, 
     [Location] NVARCHAR(100) NOT NULL, 
     [Responsible] NVARCHAR(50) NOT NULL,
-	[Block] bit NOT NULL
+	[Block] bit NOT NULL, 
+    CONSTRAINT [PK_Project] PRIMARY KEY ([ID])
 )
