31426b38a155d005dd264ff1fed48088907af31b
Muneeb Ur Rehman <munni7474@gmail.com>
2018-07-01 21:48:57 +0500

    make account and project table relationship

--- DB/dbo/Tables/Account.sql
+++ DB/dbo/Tables/Account.sql
@@ -2,7 +2,7 @@
     [ID]            INT           IDENTITY (1, 1) NOT NULL,
     [Code]          NVARCHAR (50) NOT NULL,
     [Name]          NVARCHAR (50) NOT NULL,
-    [ProjectCode] NVARCHAR (50) NULL,
+    [ProjectCode] INT NULL,
     [Type]          NVARCHAR (3)  NOT NULL,
     [Debit]         BIGINT        CONSTRAINT [DF_Account_Debit] DEFAULT ((0)) NOT NULL,
     [Credit]        BIGINT        CONSTRAINT [DF_Account_Credit] DEFAULT ((0)) NOT NULL,
@@ -12,6 +12,7 @@
     [Active]        BIT           CONSTRAINT [DF_Account_Active] DEFAULT ((1)) NOT NULL,
     [YearStart]     DATE          CONSTRAINT [DF_Account_YearStart] DEFAULT (getdate()) NOT NULL,
     [Created]       DATE          CONSTRAINT [DF_Account_Created] DEFAULT (getdate()) NOT NULL,
-    CONSTRAINT [PK_Account] PRIMARY KEY CLUSTERED ([ID] ASC)
+    CONSTRAINT [PK_Account] PRIMARY KEY CLUSTERED ([ID] ASC),
+	CONSTRAINT [FK_Account_Project] FOREIGN KEY([ProjectCode]) REFERENCES [dbo].[Project] ([ID])
 );
 
