d0b03249cd724c74aebc8f8f8a68a71add0c0086
Muneeb Ur Rehman <munni7474@gmail.com>
2018-09-02 20:44:56 +0500

    apply voucher number search on BPParent Grid on Bank Payments listing page

--- AcccountInventory/Transaction/BP/List.aspx.cs
+++ AcccountInventory/Transaction/BP/List.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Globalization;
@@ -20,7 +21,26 @@ namespace AcccountInventory.Transaction.BP
 
         protected void btnSearchTransaction_Click(object sender, EventArgs e)
         {
-            //TODO: Generate datatable and set data source property of gridview and call databind func
+            string query = "SELECT bpp.*,CONCAT(a.AccountCode, '-', p.Code) AS PACode FROM BPParent bpp INNER JOIN "
+                            + "Account a  ON bpp.AccountID = a.ID INNER JOIN Project p ON a.ProjectCode = p.ID";
+
+            DataTable dt = new DataTable();
+            string connectionString = ConfigurationManager.ConnectionStrings["AccountConnectionString"].ToString();
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            if (!string.IsNullOrEmpty(txtSearch.Text.ToString()))
+            {
+                int searchTerm = Convert.ToInt32(txtSearch.Text);
+                query += " where bpp.ID=" + searchTerm + "";
+            }
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            //bind the grid
+            GridView1.DataSourceID = null;
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            con.Close();
         }
     }
 }
